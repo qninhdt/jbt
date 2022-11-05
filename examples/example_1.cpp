a6550d8a4b671b12b5b71e10af1b2a16b47a5c00
qninhdt <qndt123@gmail.com>
2022-11-06 01:55:44 +0700

    Change tab size

--- examples/example_1.cpp
+++ examples/example_1.cpp
@@ -5,18 +5,18 @@
 using json = nlohmann::json;
 
 int main() {
-	std::ofstream out("D:/github/jbt/lmao.dat");
-	jbt::write_string(out, "1234567890123");
-	jbt::write_string(out, "abc");
-	jbt::write_string(out, "1dsdsadsad0123");
-	out.close();
-
-	jbt::ushort_tag tag;
-	std::ifstream f("example.json");
-	json data = json::parse(f);
-	std::ifstream in("D:/github/jbt/lmao.dat");
-	std::string s;
-	jbt::read_string(in, s);
-	std::cout << s.size() << ' ' << s;
-	in.close();
+    std::ofstream out("D:/github/jbt/lmao.dat");
+    jbt::write_string(out, "1234567890123");
+    jbt::write_string(out, "abc");
+    jbt::write_string(out, "1dsdsadsad0123");
+    out.close();
+    
+    jbt::ushort_tag tag;
+    std::ifstream f("example.json");
+    json data = json::parse(f);
+    std::ifstream in("D:/github/jbt/lmao.dat");
+    std::string s;
+    jbt::read_string(in, s);
+    std::cout << s.size() << ' ' << s;
+    in.close();
 }
